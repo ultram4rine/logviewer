cfe9723b923a3c4c5cf219df4980d0882301220c
ultram4rine <42237599+ultram4rine@users.noreply.github.com>
2019-06-04 21:51:45 +0400

    Added period flag

--- console/main.go
+++ console/main.go
@@ -19,19 +19,21 @@ const (
 )
 
 var (
-	t    = time.Now()
-	name = kingpin.Flag("switch", "Switch which log need view").Short('s').Default().Required().String()
-	date = kingpin.Flag("date", "Date of logs").Short('d').Default(t.Format("2006-01-02")).String()
-	rows = kingpin.Flag("rows", "Number of rows to view").Short('r').Default("40").Int()
+	t      = time.Now()
+	name   = kingpin.Flag("switch", "Switch which log need view").Short('s').Default().Required().String()
+	date   = kingpin.Flag("date", "Date of logs").Short('d').Default(t.Format("2006-01-02")).String()
+	rows   = kingpin.Flag("rows", "Number of rows to view").Short('r').Default("40").Int()
+	period = kingpin.Flag("period", "Period of last logs").Short('p').Default("30m").String()
 )
 
 func main() {
 	kingpin.Parse()
 
 	var (
-		n       = *name
-		d       = *date
-		r       = *rows
+		n = *name
+		d = *date
+		r = *rows
+		//p = *period
 		logPath = "/var/log/remote/" + n + "/" + d
 	)
 
