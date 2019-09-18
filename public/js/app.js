56bb73e84c6192f5c9c4fa3794f3c148500e128f
ultram4rine <42237599+ultram4rine@users.noreply.github.com>
2019-09-18 15:44:56 +0400

    Deleted choosing date

--- public/js/app.js
+++ public/js/app.js
@@ -1,18 +1,17 @@
 function send() {
-    var wrap = document.getElementById('responce');
+  var wrap = document.getElementById("responce");
 
-    var date = document.getElementById('date').value;
-    var ip = document.getElementById('name').value;
-    var time = document.getElementById('time').value;
-    var req = 'date=' + date + '&name=' + ip + '&time=' + time;
+  var name = document.getElementById("name").value;
+  var time = document.getElementById("time").value;
+  var req = "name=" + name + "&time=" + time;
 
-    var xhr = new XMLHttpRequest();
-    xhr.open('POST', '/get');
-    xhr.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded');
-    xhr.onload = function() {
-        if (xhr.status == 200) {
-            wrap.innerText = xhr.responseText
-        }
+  var xhr = new XMLHttpRequest();
+  xhr.open("POST", "/get");
+  xhr.setRequestHeader("Content-Type", "application/x-www-form-urlencoded");
+  xhr.onload = function() {
+    if (xhr.status == 200) {
+      wrap.innerText = xhr.responseText;
     }
-    xhr.send(req);
+  };
+  xhr.send(req);
 }
