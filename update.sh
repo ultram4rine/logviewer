a999ef50ca5ed5393f2188fbd9b305ca2cc81535
ivan <dfeqcn11@gmail.com>
2019-09-24 16:40:36 +0400

    Changed service, update.sh, rename main.go file

--- update.sh
+++ update.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
 go build main.go
-rm /var/www/logviewer/main
-cp -r main conf.json public/ /var/www/logviewer/
+rm /var/www/logviewer/logviewer
+cp -r conf.json public/ /var/www/logviewer/
+cp -f logviewer /usr/local/bin/
